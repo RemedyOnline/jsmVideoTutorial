c46d07ad2be544f21f4b3b7aa2a8c56f4a39ec6b
Remedy Online <agyemangmichael555@gmail.com>
2024-12-31 04:11:43 +0000

    A UI upgrade worth doing... Thanks to the WhatsApp guy doing the same project though...

--- src/components/MovieCard2.jsx
+++ src/components/MovieCard2.jsx
@@ -3,14 +3,31 @@ import PropTypes from "prop-types";
 const MovieCard2 = ({ imdbID, Poster, Title, Type, Year }) => {
 	return (
 		<section>
-			<div className="flex flex-col text-center p-2 bg-cyan-900 rounded-xl shadow-md h-full justify-between">
+			<div className="flex flex-col text-center p-2 bg-cyan-700 rounded-xl shadow-md h-full justify-start items-center relative group max-w-md">
 				<p>{imdbID}</p>
-				<img src={Poster} alt={Title} />
-				<p className="text-2xl font-semibold">{Title}</p>
+				<img src={Poster} alt={Title} className="rounded-lg h-[250px]" />
+				<p className="text-lg font-semibold text-cyan-950">{Title}</p>
 				<div className="flex justify-around text-lg font-mono">
-					<span>{Type}</span>
-					<span>{Year}</span>
+					{/* <span>{Type}</span> */}
+					<span className="text-cyan-950">{Year}</span>
 				</div>
+				<button className="absolute bottom-3 right-3 hidden group-hover:block p-1 rounded-full bg-cyan-800 transition-all duration-500 ease-in-out">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="#083344"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-eye"
+					>
+						<path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+						<circle cx="12" cy="12" r="3" />
+					</svg>
+				</button>
 			</div>
 		</section>
 	);
--- src/pages/MovieVerse2.jsx
+++ src/pages/MovieVerse2.jsx
@@ -22,41 +22,43 @@ const MovieVerse2 = () => {
 	}, []);
 
 	return (
-		<section className="bg-cyan-800 h-full mx-auto my-auto items-center justify-start flex flex-col space-y-10">
-			<div>
-				<h1 className="text-center text-2xl md:text-4xl font-bold font-mono">
-					MovieVerse
-				</h1>
-				<p className="text-sm md:text-xl my-2 italic text-center">
-					A whole universe of movies at your fingertips
-				</p>
-			</div>
-			<div>
-				<input
-					type="search"
-					name="search"
-					placeholder="Search for any movie..."
-					id="search"
-					value={searchValue}
-					onChange={(e) => setSearchValue(e.target.value)}
-					className="w-full bg-cyan-700 shadow-lg rounded-full py-2 px-4"
-				/>
-				<Search className="" onClick={() => getMovies(searchValue)} />
-			</div>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md: md:grid-cols-3 gap-5">
-				{movies?.length > 0 ? (
-					movies.map((movie) => (
-						<MovieCard2
-							key={movie.imdbID}
-							Year={movie.Year}
-							Poster={movie.Poster}
-							Title={movie.Title}
-							Type={movie.Type}
-						/>
-					))
-				) : (
-					<div>No movies available...</div>
-				)}
+		<section className="bg-cyan-800">
+			<div className=" h-full mx-auto my-auto items-center justify-start flex flex-col space-y-10 2xl:max-w-6xl">
+				<div>
+					<h1 className="text-center text-2xl md:text-4xl font-bold font-mono">
+						MovieVerse
+					</h1>
+					<p className="text-sm md:text-xl my-2 italic text-center">
+						A whole universe of movies at your fingertips
+					</p>
+				</div>
+				<div className="w-1/2 bg-cyan-600 shadow-lg rounded-full py-1 px-5 flex gap-2 text-cyan-950 items-center">
+					<input
+						type="search"
+						name="search"
+						placeholder="Search for any movie..."
+						id="search"
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+						className="w-full bg-transparent placeholder:text-cyan-950 py-2 outline-none"
+					/>
+					<Search className="" onClick={() => getMovies(searchValue)} />
+				</div>
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 px-5">
+					{movies?.length > 0 ? (
+						movies.map((movie) => (
+							<MovieCard2
+								key={movie.imdbID}
+								Year={movie.Year}
+								Poster={movie.Poster}
+								Title={movie.Title}
+								Type={movie.Type}
+							/>
+						))
+					) : (
+						<div>No movies available...</div>
+					)}
+				</div>
 			</div>
 		</section>
 	);
